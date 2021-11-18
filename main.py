e9f656c307a135cdf150c416df4ee1634482dc67
linkage001 <linkage001@users.noreply.github.com>
2021-11-18 01:21:40 +0000

    Added DB creation to main.py

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 from flask_thread import FlaskThread
+import sqlite3
 
 if __name__ == '__main__':
+
+    conn = sqlite3.connect('data.db')
+    c = conn.cursor()
+
+    c.execute('''
+              CREATE TABLE IF NOT EXISTS 'users' ( 
+              'id' INTEGER PRIMARY KEY AUTOINCREMENT, 
+              'username'  TEXT,     
+              'token'  TEXT );
+              ''')
+
+    c.execute('''
+              CREATE TABLE IF NOT EXISTS 'shoppingLists' ( 
+              'id' INTEGER PRIMARY KEY AUTOINCREMENT, 
+              'userID'  INTEGER);
+              ''')
+
+    conn.commit()
 
     ft = FlaskThread()
 
